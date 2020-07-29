ac332552a632baafd1390c4f6f1df0c486ea2a84
dekaio <47273651+dekaio@users.noreply.github.com>
2020-07-29 15:49:50 +0530

    Update SparseMatrix.c

--- SparseMatrix.c
+++ SparseMatrix.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 struct Element{
     int i;
     int j;
@@ -9,25 +9,25 @@ struct SparseMatrix{
     int n;
     int num;
     struct Element *ele;
-}
+};
 void create(struct SparseMatrix *s){
     int i;
     printf("Enter Dimensions(Rows and columns)");
-    scanf("%d%d",s->m,s->n);
+    scanf("%d%d",&s->m,&s->n);
     printf("Enter no of non zero elements");
-    scanf("%d",s->num);
+    scanf("%d",&s->num);
     s->ele = (int *)malloc(sizeof(int)*s->num);
     for (i=0;i<s->num;i++){
         printf("Enter row index, column index and element: ");
-        scanf("%d%d%d",s->ele[i]->n,s->ele[i]->j,s->ele[i]->x);
+        scanf("%d%d%d",&s->ele[i].i,&s->ele[i].j,&s->ele[i].x);
     }
 }
 void display (struct SparseMatrix s){
     int l = 0;
     for (int i=0;i<s.m;i++){
         for (int j=0;j<s.n;j++){
-            if (i==s.ele[l]->i && j=s.ele[l]->j){
-                printf("%d",s->ele[l]->x);
+            if (i==s.ele[l].i && j==s.ele[l].j){
+                printf("%d",s.ele[l].x);
                 l++;
             }
             else printf("%d",0);
@@ -36,7 +36,7 @@ void display (struct SparseMatrix s){
 }
 int main()
 {   struct SparseMatrix s;
-    void create(&s);
+    create(&s);
     display(s);
     return 0;
 }
