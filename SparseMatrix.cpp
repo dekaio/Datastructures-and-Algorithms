82c7a2f18107fd058d49f9825e0bbbf59730fcd9
dekaio <47273651+dekaio@users.noreply.github.com>
2020-07-31 13:08:20 +0530

    Addition not working

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -24,34 +24,77 @@ class Sparse: public Element{
         ~Sparse(){
             delete [] ele;
         }
-        void read(){
-            cout<<"Enter number of non zero elements";
-            cin>>num;
-            cout <<"Enter row,column,val of non zero elements";
-            for(int i=0;i<num;i++){
-              cin>>ele[i].i>>ele[i].j>>ele[i].x; 
-              cout<<"\n";
-            }
-        }
-        void display(){
-            int k;
-            for (int i=0;i<m;i++){
-                for (int j=0;j<n;j++){
-                    if (ele[k].i==i && ele[k].j==j){
-                        cout<<ele[k++].x;
-                    }
-                    else cout<<0;
-                }
-                cout<<"\n";
-            }
-        }
-    
+        Sparse operator+(Sparse &s);
+        friend istream & operator>>(istream &is, Sparse &s);
+        friend ostream & operator<<(ostream &os, Sparse &s);
 };
+Sparse Sparse::operator+(Sparse &s){
+    int i=0,j=0,k=0;
+    //if (m!=s.m || n!=s.n){
+      //  return NULL;
+    //}
+    Sparse *adm = new Sparse(m,n,num+s.m);
+    while(i<num && j<s.num){
+        if (ele[i].i<s.ele[j].i){
+            adm->ele[k++] = ele[i++];
+        }
+        else if (ele[i].i > s.ele[j].i){
+            adm->ele[k++] = s.ele[j++];
+        }
+        else {
+            if (ele[i].j<s.ele[j].j){
+                adm->ele[k++] = ele[i++];
+            }
+            else if (ele[i].j>s.ele[j].j){
+                adm->ele[k++] = s.ele[j++];
+            }
+            else{
+                adm->ele[k] = ele[i];
+                adm->ele[k++].x = ele[i++].x + s.ele[j++].x;
+            }
+        }
+        for (;i<num;i++)adm->ele[k++] = ele[i++];
+        for (;j<s.num;j++)adm->ele[k++] = s.ele[j++];
+        adm->num = k;
+    }
+    return *adm;
+}
+istream & operator>>(istream &is, Sparse &s){
+    cout<<"Enter number of non zero elements";
+    cin>>s.num;
+    cout <<"Enter row,column,val of non zero elements";
+    for(int i=0;i<s.num;i++){
+      cin>>s.ele[i].i>>s.ele[i].j>>s.ele[i].x; 
+      cout<<"\n";
+    }
+    return is;
+}
+ostream & operator<<(ostream &os, Sparse &s){
+    int k;
+    for (int i=0;i<s.m;i++){
+        for (int j=0;j<s.n;j++){
+            if (s.ele[k].i==i && s.ele[k].j==j){
+                cout<<s.ele[k++].x;
+            }
+            else cout<<0;
+        }
+        cout<<"\n";
+    }
+    return os;
+}
+    
+
 int main()
 {
-    Sparse mat(4,4,4);
-    mat.read();
-    mat.display();
-
+    Sparse mat1(4,4,4);
+    Sparse mat2(4,4,4);
+    cout<<"Enter matrix 1\n";
+    cin>>mat1;
+    cout<<"Enter matrix 2\n";
+    cin>>mat2;
+    Sparse sum = mat1+mat2;
+    cout<<"First matrix"<<endl<<mat1;
+    cout<<"Second matrix"<<endl<<mat2;
+    cout<<sum;
     return 0;
 }
