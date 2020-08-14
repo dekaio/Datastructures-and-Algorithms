3b1df1a20f7a9e23b73aa6fb398db6c8425ec721
dekaio <47273651+dekaio@users.noreply.github.com>
2020-08-14 14:44:54 +0530

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,1 +1,54 @@
-
+# include<iostream>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node *next;
+};
+class LinkedList{
+    private:
+        Node *first;
+    public:
+        LinkedList(){first = NULL;}
+        LinkedList(int A[], int n);
+        ~LinkedList();
+        void Display();
+        void Insert(int index, int ele);
+        void Delete(int index);
+        int Length();
+};
+LinkedList::LinkedList(int A[], int n){
+    Node *last = new Node;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    for (int i=1;i<n;i++){
+        Node *temp = new Node;
+        temp->data = A[i];
+        temp->next = NULL;
+        last->next = temp;
+        last = last->next;
+    }    
+}
+LinkedList::~LinkedList(){
+    Node *temp = first;
+    while(first){
+         first = first->next;
+         delete temp;
+         temp = first;
+    }
+}
+void LinkedList::Display(){
+    class Node *temp = this->first;
+    while(temp){
+        cout<<temp->data<<endl;
+        temp = temp->next;
+    }
+}
+int main(){
+    int A[5] = {1,2,3,4,5};
+    class LinkedList list(A,5);
+    list.Display();
+    return 0;
+}
