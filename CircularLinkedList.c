c88cde197bba4afb2c06f3c84abf82cab2710a18
dekaio <47273651+dekaio@users.noreply.github.com>
2020-08-17 16:21:33 +0530

    Update CircularLinkedList.c

--- CircularLinkedList.c
+++ CircularLinkedList.c
@@ -1,1 +1,43 @@
 
+/******************************************************************************
+
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+}*Head = NULL;
+
+void CreateNode(int A[], int n){
+    struct Node *p;int i = 0;
+    Head = (struct Node *)malloc(sizeof(struct Node));
+    p = Head;
+    p->data = A[i++];
+    p->next = Head;
+    int k,j;
+    while(i<n){
+        struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+        temp->data = A[i++];
+        temp->next = p->next;
+        p->next = temp;
+        p = temp;
+        j = p->data;
+        k = i;
+    }
+}
+void Display(){
+    struct Node *p;
+    p = Head;
+    do{
+        printf("%d\n",p->data);
+        p = p->next;
+    }while(p!=Head);
+}
+int main()
+{
+    int A[5] = {1,2,3,4,5};
+    CreateNode(A,5);
+    printf("Displaying elements:\n");
+    Display();
+    return 0;
+}
