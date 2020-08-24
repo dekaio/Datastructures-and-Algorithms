db64596a2e3a106096145d947bb0b2c906fd31aa
dekaio <47273651+dekaio@users.noreply.github.com>
2020-08-24 21:58:56 +0530

    Update BubbleSort.c

--- BubbleSort.c
+++ BubbleSort.c
@@ -1,15 +1,20 @@
 
 #include <stdio.h>
 void Swap(int *a, int *b){
-    int *temp = a;
-    a = b;
-    b = a;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void Display(int A[], int n){
+    for (int i=0;i<n;i++){
+        printf("%d",A[i]);
+    }
 }
 void BubbleSort(int A[], int n){
     int flag = 0;
     for (int i=0;i<n-1;i++){
         for (int j=0;j<n-i-1;j++){
-            if (A[j]>A[j++]) {
+            if (A[j]>A[j+1]) {
                 Swap(&A[j],&A[j++]);
                 flag = 1;
             }
@@ -17,11 +22,7 @@ void BubbleSort(int A[], int n){
         if (flag==0) return;
     }
 }
-void Display(int A[], int n){
-    for (int i=0;i<n;i++){
-        printf("%d",A[i]);
-    }
-}
+
 int main()
 {
     int A[] = {3,5,2,6,4};
