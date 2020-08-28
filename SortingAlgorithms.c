4b1b7dc321a51635c45ae2b99339ded0c876d286
dekaio <47273651+dekaio@users.noreply.github.com>
2020-08-28 20:43:37 +0530

    Added insertion sort

--- SortingAlgorithms.c
+++ SortingAlgorithms.c
@@ -22,7 +22,18 @@ void BubbleSort(int A[], int n){
         if (flag==0) return;
     }
 }
-
+void InsertionSort(int A[], int n){
+    int j,x;
+    for (i=1;i<n;i++){
+        j = i-1;
+        x = A[i];
+        while(j>-1 && A[j]>x){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = x;
+    }
+}
 int main()
 {
     int A[] = {3,5,2,6,4};
