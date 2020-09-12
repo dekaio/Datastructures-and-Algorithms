95c527a0e156954005a8e94bb2418a3ce34ed936
dekaio <47273651+dekaio@users.noreply.github.com>
2020-09-12 19:32:57 +0530

    Queue using arrays

--- Queue.c
+++ Queue.c
@@ -47,3 +47,38 @@ int main()
     printf("Hello World");
     return 0;
 }
+
+
+------------------------------------------------------------
+  #include <stdio.h>
+struct Queue{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+void create(struct Queue *q, int size){
+    q->size = size;
+    q->front = q->rear=0;
+    q->Q= (int *)malloc(q->size*sizeof(int));
+}
+void enqueue(struct Queue *q, int x){
+    if ((q->rear+1)%q->size==q->front){
+        printf("Queue is full");
+    }
+    else{
+        q->rear = (q->rear+1)%q->size;
+        q->Q[q->rear]=x;
+    }
+}
+int dequeue(struct Queue *q){
+    int x =-1;
+    if (q->front==q->rear){
+        printf("Queue is empty");
+    }
+    else{
+        q->front = (q->front+1)%q->size;
+        x=q->Q[q->front];
+    }
+    return x;
+}
