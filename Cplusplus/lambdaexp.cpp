37ced775ec0b7cf8ca9a18d68236d350c96db9c6
dekaio <47273651+dekaio@users.noreply.github.com>
2020-10-21 16:34:55 +0530

    Create lambdaexp.cpp

---
+++ Cplusplus/lambdaexp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//[capture-list](parameter_list)->returntype{body}
+int main()
+{
+    [](){cout<<"Hello";}();
+    [](int x, int y){cout<<"sum:"<<x+y;}(10,4);
+    auto x = [](int x, int y){return x+y;}(10,4);
+    int a = [](int x, int y)->int{return x+y;}(1)
+    cout<<"a is"<<a<<endl;
+    auto f = [](){cout<<"Hello";};f();
+    int a = 10;
+    int b = 5;
+    [&a,&b](){cout<<a+b<<endl;}();
+    return 0;
+}
