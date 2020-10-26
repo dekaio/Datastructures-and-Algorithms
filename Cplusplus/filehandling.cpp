d706853f164716a36c7981f7caaf251146548a7d
dekaio <47273651+dekaio@users.noreply.github.com>
2020-10-26 12:29:23 +0530

    Update filehandling.cpp

--- Cplusplus/filehandling.cpp
+++ Cplusplus/filehandling.cpp
@@ -6,5 +6,17 @@ int main()
     ofstream outfile('mytxt', ios::app);
     outfile<<"Hello"<<endl;
     outfile.close();
+    ifstream infile;
+    infile.open('mytxt', ios::in);
+    string st; int x;
+    if (!infile):
+        cout<<"file cannot be opened";
+    else{
+        infile>>st;
+        infile>>x;
+        cout<<st<<" "<<x;
+        if(infile.eof()) cout<<"end of file";
+    }
+    infile.close();
     return 0;
 }
